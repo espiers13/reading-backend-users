f28156c9ce22427eae1e5bec5fe04352f3c82e1b
Emily <e.spiers13@gmail.com>
2025-04-04 10:11:45 +0100

    updated favourites data for testing

--- db/data/test-data/favourites.js
+++ db/data/test-data/favourites.js
@@ -1,14 +1,14 @@
 module.exports = [
-  { user_id: 2, isbn: "9780140449136" },
-  { user_id: 3, isbn: "9780062315007" },
-  { user_id: 2, isbn: "9781501173219" },
-  { user_id: 2, isbn: "9780143127741" },
-  { user_id: 3, isbn: "9780307277671" },
-  { user_id: 3, isbn: "9781400032716" },
-  { user_id: 4, isbn: "9780307887443" },
-  { user_id: 4, isbn: "9780735219090" },
-  { user_id: 5, isbn: "9780345803481" },
-  { user_id: 5, isbn: "9780743273565" },
-  { user_id: 6, isbn: "9780439139601" },
-  { user_id: 6, isbn: "9780061120084" },
+  { user_id: 2, isbn: "9780140449136" }, // Crime and Punishment
+  { user_id: 3, isbn: "9780062315007" }, // The Alchemist
+  { user_id: 2, isbn: "9781501173219" }, // It
+  { user_id: 2, isbn: "9780143127741" }, // The Boys in the Boat
+  { user_id: 3, isbn: "9780307277671" }, // The Black Swan
+  { user_id: 3, isbn: "9781400032716" }, // One Hundred Years of Solitude
+  { user_id: 4, isbn: "9780307887443" }, // The Lean Startup
+  { user_id: 4, isbn: "9780735219090" }, // Atomic Habits
+  { user_id: 5, isbn: "9780345803481" }, // Fifty Shades of Grey
+  { user_id: 5, isbn: "9780743273565" }, // The Great Gatsby
+  { user_id: 6, isbn: "9781635574966" }, // Throne of Glass by Sarah J. Maas
+  { user_id: 6, isbn: "9781619634442" }, // A Court of Mist and Fury by Sarah J. Maas
 ];
